953760f8a770fbf901c63e817bc136f159e651da
Febri Hidayan <febrihidayan20@gmail.com>
2021-08-25 23:16:15 +0700

    updated validate password roles

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Validation\Rules\Password;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,6 +25,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Password::defaults( function() {
+            return Password::min(6)
+                ->mixedCase()
+                ->uncompromised();
+        });
     }
 }
