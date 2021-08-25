5e787cb954e52cbd86158fed84f427f4981d6d28
Febri Hidayan <febrihidayan20@gmail.com>
2021-08-25 23:01:06 +0700

    fix: color message

--- resources/views/components/auth-session-status.blade.php
+++ resources/views/components/auth-session-status.blade.php
@@ -1,7 +1,7 @@
 @props(['status'])
 
 @if ($status)
-    <div {{ $attributes->merge(['class' => 'font-medium text-sm ' . (session('success') ? 'text-green-600' : 'text-red-600')]) }}>
+    <div {{ $attributes->merge(['class' => 'font-medium text-sm ' . (session('status') ? 'text-green-600' : 'text-red-600')]) }}>
         {{ $status }}
     </div>
 @endif
